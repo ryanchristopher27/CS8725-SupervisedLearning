6312b9380362f9009527516c8facc89a1b4c3cdb
Ryan Christopher <ryandchristopher27@gmail.com>
2023-10-06 00:16:47 -0500

    added accruacy plot

--- assignment3/logistic_regression.py
+++ assignment3/logistic_regression.py
@@ -1,4 +1,5 @@
 from math import exp
+import math
 from ucimlrepo import fetch_ucirepo 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -43,12 +44,12 @@ def main():
     # plt.show()
 
     # The accuracy
-    accuracy = 0
-    for i in range(len(Y_test_pred)):
-        if Y_test_pred[i] == Y_test.iloc[i]:
-            accuracy += 1
-    print(f"Accuracy = {accuracy / len(Y_test_pred)}")
-
+    accuracy = get_accuracy(Y_test_pred, Y_test)
+    # accuracy = 0
+    # for i in range(len(Y_test_pred)):
+    #     if Y_test_pred[i] == Y_test.iloc[i]:
+    #         accuracy += 1
+    print(f"Accuracy = {accuracy}")
 
 
 
@@ -60,7 +61,10 @@ def logistic_regression(X :pd.DataFrame, y :pd.DataFrame, features :[]) -> []:
     # weights = [0 for x in range(len(features))]
 
     learning_rate = 0.001
-    epochs = 300
+    epochs = 200
+
+    errors = []
+    accuracies = []
 
     X_normalized = normalize(X)
 
@@ -68,9 +72,19 @@ def logistic_regression(X :pd.DataFrame, y :pd.DataFrame, features :[]) -> []:
     for epoch in range(epochs):
         y_prediction = sigmoid(X_normalized, weights, features)
 
+        error = get_error(y_prediction, y)
+        errors.append(error)
+
+        y_prediction_made = [1 if p >= 0.5 else 0 for p in y_prediction]
+        accuracy = get_accuracy(y_prediction_made, y)
+        accuracies.append(accuracy)
+
         derivatives = get_derivatives(X_normalized, y, y_prediction, features)
 
         weights = update_weights(weights, derivatives, learning_rate)
+
+    plot_error(errors, epochs)
+    plot_accuracy(accuracies, epochs)
 
     return weights
 
@@ -89,6 +103,23 @@ def sigmoid(X :pd.DataFrame, weights :[], features :[]) -> np.array:
     return np.array(predictions)
     # return np.array([1 / (1 + exp(-sum([weights[i] * x[feature] for i, feature in enumerate(features)]))) for x in X])
 
+def get_error(y_pred :np.array, y :pd.DataFrame) -> float:
+    errors = y_pred - y
+    squared_errors = errors ** 2
+
+    mean_squared_error = sum(squared_errors) / len(errors)
+
+    return mean_squared_error
+
+def get_accuracy(y_pred :np.array, y:pd.DataFrame) -> float:
+    correct_predictions = 0
+    for i in range(len(y_pred)):
+        if y_pred[i] == y.iloc[i]:
+            correct_predictions += 1
+
+    accuracy = correct_predictions / len(y_pred)
+    return accuracy
+    # print(f"Accuracy = {accuracy / len(y_pred)}")
 
 def get_derivatives(X :pd.DataFrame, y :pd.DataFrame, y_pred: np.array, features :[]) -> []:
         derivatives = []
@@ -108,6 +139,23 @@ def update_weights(weights :[], derivatives :[], learning_rate :float) -> []:
 
 def normalize(X :pd.DataFrame) -> pd.DataFrame:
     return X - X.mean()
+
+def plot_error(errors :[], epochs :int) -> None:
+    x = np.arange(1, epochs + 1)
+    plt.plot(x, errors)
+    plt.xlabel('Epochs')
+    plt.ylabel('Mean Squared Error')
+    plt.title('Mean Squared Error per Epoch')
+    plt.show()
+
+def plot_accuracy(accuracies :[], epochs :int) -> None:
+    x = np.arange(1, epochs + 1)
+    plt.plot(x, accuracies)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy per Epoch')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
